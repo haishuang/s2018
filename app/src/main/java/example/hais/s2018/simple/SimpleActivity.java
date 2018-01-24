f3364c4142bf82632b80a0978313c56e0bc634f1
haishuang <623691640@qq.com>
2018-01-24 16:14:49 +0800

    测试2

--- app/src/main/java/example/hais/s2018/simple/SimpleActivity.java
+++ app/src/main/java/example/hais/s2018/simple/SimpleActivity.java
@@ -1,8 +1,15 @@
 package example.hais.s2018.simple;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.drawable.Drawable;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -13,7 +20,9 @@ public class SimpleActivity extends BaseActivity {
 
     @Bind(R.id.tv_01)
     TextView tv01;
-
+    @Bind(R.id.tv_battery_info)
+    TextView tvBatteryInfo;
+    private BatteryChangedReceiver batteryChangedReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +32,135 @@ public class SimpleActivity extends BaseActivity {
         initView();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unRegistBatter();
+    }
+
     private void initView() {
         //1.View四周设置图片
         Drawable drawableCai = getResources().getDrawable(R.drawable.simple_zan);
         // 调用setCompoundDrawables时，必须调用Drawable.setBounds()方法,否则图片不显示
         drawableCai.setBounds(0, 0, drawableCai.getMinimumWidth(), drawableCai.getMinimumHeight());
         tv01.setCompoundDrawables(drawableCai, drawableCai, drawableCai, drawableCai); //设置左图标
+        batteryChangedReceiver=new BatteryChangedReceiver();
+        registBatter();
+    }
+
+    //注册电量监听的Broadcastreceiver
+    public void registBatter(){
+        IntentFilter intentFilter=getFilter();
+        registerReceiver(batteryChangedReceiver,intentFilter);
+        Toast.makeText(this, "电量变化的receiver已经注册成功",Toast.LENGTH_SHORT).show();
+    }
+    //取消注册电量监听的Broadcastreceiver
+    public void unRegistBatter(){
+        unregisterReceiver(batteryChangedReceiver);
+        Toast.makeText(this,"电量变化的receiver已经取消注册",
+                Toast.LENGTH_SHORT).show();
+    }
+    ///获取IntentFilter对象
+    private IntentFilter getFilter() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+        filter.addAction(Intent.ACTION_BATTERY_OKAY);
+        return filter;
+    }
+
+    public class BatteryChangedReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // TODO Auto-generated method stub
+            final String action = intent.getAction();
+            String batteryInfo = "";
+            if (action.equalsIgnoreCase(Intent.ACTION_BATTERY_CHANGED)) {
+                // 当前电池的电压
+                int voltage = intent.
+                        getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
+                // 电池的健康状态
+                int health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, -1);
+                batteryInfo = "健康状态:";
+                switch (health) {
+                    case BatteryManager.BATTERY_HEALTH_GOOD:
+                        batteryInfo += "很好";
+                    case BatteryManager.BATTERY_HEALTH_COLD:
+                        batteryInfo += "BATTERY_HEALTH_COLD";
+                        break;
+                    case BatteryManager.BATTERY_HEALTH_DEAD:
+                        batteryInfo += "BATTERY_HEALTH_DEAD";
+                        break;
+                    case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                        batteryInfo += "BATTERY_HEALTH_OVERHEAT";
+                        break;
+                    case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                        batteryInfo += "BATTERY_HEALTH_OVER_VOLTAGE";
+                        break;
+                    case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                        batteryInfo += "BATTERY_HEALTH_UNKNOWN";
+                        break;
+                    case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                        batteryInfo += "BATTERY_HEALTH_UNSPECIFIED_FAILURE";
+                        break;
+                    default:
+                        break;
+                }
+                // 电池当前的电量, 它介于0和 EXTRA_SCALE之间
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                batteryInfo+="\n当前电量:"+level;
+
+                // 电池电量的最大值
+                int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                batteryInfo+="\n最大电量:"+scale;
+                // 当前手机使用的是哪里的电源
+                int pluged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+                batteryInfo+="\n耗电来源:"+(pluged==BatteryManager.BATTERY_PLUGGED_AC?"AC":"USB");
+
+                batteryInfo+="\n电量状态:"+scale;
+                int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+                switch (status) {
+                    case BatteryManager.BATTERY_STATUS_CHARGING:
+                        // 正在充电
+                        batteryInfo+="正在充电";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                        batteryInfo+="BATTERY_STATUS_DISCHARGING";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_FULL:
+                        // 充满
+                        batteryInfo+="充满";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                        // 没有充电
+                        batteryInfo+="没有充电";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_UNKNOWN:
+                        // 未知状态
+                        batteryInfo+="未知状态";
+                        break;
+                    default:
+                        break;
+                }
+                // 电池使用的技术。比如，对于锂电池是Li-ion
+                String technology = intent.
+                        getStringExtra(BatteryManager.EXTRA_TECHNOLOGY);
+                // 当前电池的温度
+                int temperature = intent.
+                        getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1);
+                String str= "voltage = " + voltage + " technology = "
+                        + technology + " temperature = " + temperature;
+                batteryInfo+="\n"+str;
+            } else if (action.equalsIgnoreCase(Intent.ACTION_BATTERY_LOW)) {
+                // 表示当前电池电量低
+            } else if (action.equalsIgnoreCase(Intent.ACTION_BATTERY_OKAY)) {
+                // 表示当前电池已经从电量低恢复为正常
+                System.out.println(
+                        "BatteryChangedReceiver ACTION_BATTERY_OKAY---");
+            }
+
+            tvBatteryInfo.setText(batteryInfo);
+        }
+
     }
 }
