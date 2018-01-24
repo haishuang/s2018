4f234a8d071a3c54dc058b1e14c8b69fb502f493
haishuang <623691640@qq.com>
2018-01-24 17:06:33 +0800

    测试提交

---
+++ app/src/main/java/example/hais/s2018/mine/LoginActivity.java
@@ -0,0 +1,16 @@
+package example.hais.s2018.mine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import example.hais.s2018.R;
+import example.hais.s2018.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine_login);
+    }
+}
