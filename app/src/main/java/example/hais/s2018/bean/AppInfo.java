e9398b6480780d337f7a0fe3668ab36bfb05a2c6
xhg <623691640@qq.com>
2018-03-13 10:48:29 +0800

    测试

---
+++ app/src/main/java/example/hais/s2018/bean/AppInfo.java
@@ -0,0 +1,37 @@
+package example.hais.s2018.bean;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Administrator on 2018/3/13.
+ */
+
+public class AppInfo {
+    String appName;
+    String packageName;
+    Drawable drawable;
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public Drawable getDrawable() {
+        return drawable;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        this.drawable = drawable;
+    }
+}
